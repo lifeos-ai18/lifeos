d0f6d0d6e027971e141fb8f892384a106b64202b
lifeos-ai18 <mehtakavish881@gmail.com>
2026-06-09 23:28:30 +0530

    Update lifeos.py

--- lifeos.py
+++ lifeos.py
@@ -13,10 +13,10 @@ layout="wide"
 
 # -----------------------
 
-if "xp" not in st.session_state:
+ "xp" not in st.session_state:
 st.session_state.xp = 0
 
-if "missions" not in st.session_state:
+ "missions" not in st.session_state:
 st.session_state.missions = []
 
 # -----------------------
